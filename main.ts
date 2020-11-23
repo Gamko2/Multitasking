93ff55fe7f5ccc8f8517b4a395089ccc8cdf3d2d
gamko2 <3077-916@online.de>
2020-11-23 10:27:08 +0100

    Using for each instead of for loop & slight syntax update

--- main.ts
+++ main.ts
@@ -2,21 +2,21 @@ function findTaskTime(tasks: number[], cooldown: number) {
     let currentTasks = new Array();
     let result: number = 0;
     let cooldownTimer = cooldown
-    for (let i = 0; i < tasks.length; i++) {
-        if (currentTasks.includes(tasks[i])) {
-            result = result + currentTasks.length + cooldownTimer;
+    tasks.forEach(task => {
+        if (currentTasks.includes(task)) {
+            result += currentTasks.length + cooldownTimer;
             currentTasks = [];
-            currentTasks.push(tasks[i])
+            currentTasks.push(task)
         }
         else {
-            currentTasks.push(tasks[i]);
+            currentTasks.push(task);
             if (cooldownTimer > 0 && currentTasks.length > 1) {
-                cooldownTimer = cooldownTimer - 1;
+                 cooldownTimer -= 1;
             }
         }
-    }
+    })
     return result + currentTasks.length;
 }
-console.log(findTaskTime([1, 1, 2, 1, 2], 2));
+console.log(findTaskTime([1, 2, 3, 1, 2, 3, 1, 2, 3], 5));
 
 
